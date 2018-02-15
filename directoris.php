6bacd18160cbf1ad90e24ddb6c5c7a7756a405ba
JTorrus <jtorrusgalindo@gmail.com>
2018-02-15 22:10:54 +0100

    Project ready

--- directoris.php
+++ directoris.php
@@ -1,28 +1,28 @@
 <html>
-	<head>
-		<title>Fusió de fitxers</title>		
-	</head>
-	<body>
-		<?php	
-		include ("fusio_directoris.inc");
-		include ("constants.inc");
+<head>
+    <title>Fusió de fitxers</title>
+</head>
+<body>
+<?php
+include("fusio_directoris.inc");
+include("constants.inc");
 
-		llistat_directoris(DIRECTORI_BASE);
-		?>
-		<br>
-		<div>Escriviu el nom dels dos directoris a fusionar</div>
-		<br>
-		<form method="post" action="fusiona.php">
-			<div>Nom del primer directori a fusionar:</div>
-			<div><input type="text" name="dir1" size=50></div>
-			<br>
-			<div>Nom del segon directori a fusionar:</div>
-			<div><input type="text" name="dir2" size=50></div>
-			<br>
-			<div>Nom de directori fusionat:</div>
-			<div><input type="text" name="dirfusionat" size=50></div>
-			<br>
-			<div><input type="submit" name="submit" value="fusiona"></div>
-		</form>
-	</body>
+llistat_directoris(DIRECTORI_BASE);
+?>
+<br>
+<div>Escriviu el nom dels dos directoris a fusionar</div>
+<br>
+<form method="post" action="fusiona.php">
+    <div>Nom del primer directori a fusionar:</div>
+    <div><input type="text" name="dir1" size=50></div>
+    <br>
+    <div>Nom del segon directori a fusionar:</div>
+    <div><input type="text" name="dir2" size=50></div>
+    <br>
+    <div>Nom de directori fusionat:</div>
+    <div><input type="text" name="dirfusionat" size=50></div>
+    <br>
+    <div><input type="submit" name="submit" value="fusiona"></div>
+</form>
+</body>
 </html>
--- fusiona.php
+++ fusiona.php
@@ -1,44 +1,50 @@
 <?php
-    include("fusio_directoris.inc");
-    include("constants.inc");
+include("fusio_directoris.inc");
+include("constants.inc");
 
-    $inputMergedDir = DIRECTORI_BASE."/".$_POST["dirfusionat"];
+$inputMergedDir = DIRECTORI_BASE . "/" . $_POST["dirfusionat"];
 
-    if (file_exists($inputMergedDir)) {
-        esborra_directori($inputMergedDir);
-        mergeDirs();
-    } else {
-        mergeDirs();
-    }
+if (file_exists($inputMergedDir)) {
+    esborra_directori($inputMergedDir);
+    mergeDirs();
+} else {
+    mergeDirs();
+}
 
-    function mergeDirs() {
-        $firstDir = DIRECTORI_BASE."/".$_POST["dir1"];
-        $secondDir = DIRECTORI_BASE."/".$_POST["dir2"];
-        $inputMergedDir = DIRECTORI_BASE."/".$_POST["dirfusionat"];
+function mergeDirs()
+{
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $firstDir = DIRECTORI_BASE . "/" . $_POST["dir1"];
+        $secondDir = DIRECTORI_BASE . "/" . $_POST["dir2"];
+        $inputMergedDir = DIRECTORI_BASE . "/" . $_POST["dirfusionat"];
 
-        $filesFirstDir = llistat_fitxers($firstDir);
-        $filesSecondDir = llistat_fitxers($secondDir);
+        if (file_exists($firstDir) && file_exists($secondDir)) {
+            $filesFirstDir = llistat_fitxers($firstDir);
+            $filesSecondDir = llistat_fitxers($secondDir);
 
-        $concatSecondDir = $_POST["dir2"];
+            $concatSecondDir = $_POST["dir2"];
 
-        mkdir($inputMergedDir);
+            if ($inputMergedDir) {
+                foreach ($filesFirstDir as $item) {
+                    $source = $firstDir . "/" . $item;
+                    $dest = $inputMergedDir . "/" . $item;
 
-        foreach ($filesFirstDir as $item) {
-            $source = $firstDir."/".$item;
-            $dest = $inputMergedDir."/".$item;
+                    copy($source, $dest);
+                }
 
-            copy($source, $dest);
-        }
+                foreach ($filesSecondDir as $item) {
+                    $source = $secondDir . "/" . $item;
+                    $dest = $inputMergedDir . "/" . $item;
 
-        foreach ($filesSecondDir as $item) {
-            $source = $secondDir."/".$item;
-            $dest = $inputMergedDir."/".$item;
+                    if (conte_fitxer($dest, $inputMergedDir)) {
+                        $dest = $inputMergedDir . "/" . $item . "_" . $concatSecondDir;
+                    }
 
-            if (conte_fitxer($dest, $inputMergedDir)) {
-                $dest = $inputMergedDir."/".$item."_".$concatSecondDir;
+                    copy($source, $dest);
+                }
             }
-
-            copy($source, $dest);
         }
     }
+}
+
 ?>
