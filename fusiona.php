205e11ed7774a02c990950f9a72b451a52119c3c
JTorrus <jtorrusgalindo@gmail.com>
2018-02-15 22:03:01 +0100

    Copying and removing methods implemented

--- fusiona.php
+++ fusiona.php
@@ -1,3 +1,44 @@
 <?php
+    include("fusio_directoris.inc");
+    include("constants.inc");
 
+    $inputMergedDir = DIRECTORI_BASE."/".$_POST["dirfusionat"];
+
+    if (file_exists($inputMergedDir)) {
+        esborra_directori($inputMergedDir);
+        mergeDirs();
+    } else {
+        mergeDirs();
+    }
+
+    function mergeDirs() {
+        $firstDir = DIRECTORI_BASE."/".$_POST["dir1"];
+        $secondDir = DIRECTORI_BASE."/".$_POST["dir2"];
+        $inputMergedDir = DIRECTORI_BASE."/".$_POST["dirfusionat"];
+
+        $filesFirstDir = llistat_fitxers($firstDir);
+        $filesSecondDir = llistat_fitxers($secondDir);
+
+        $concatSecondDir = $_POST["dir2"];
+
+        mkdir($inputMergedDir);
+
+        foreach ($filesFirstDir as $item) {
+            $source = $firstDir."/".$item;
+            $dest = $inputMergedDir."/".$item;
+
+            copy($source, $dest);
+        }
+
+        foreach ($filesSecondDir as $item) {
+            $source = $secondDir."/".$item;
+            $dest = $inputMergedDir."/".$item;
+
+            if (conte_fitxer($dest, $inputMergedDir)) {
+                $dest = $inputMergedDir."/".$item."_".$concatSecondDir;
+            }
+
+            copy($source, $dest);
+        }
+    }
 ?>
